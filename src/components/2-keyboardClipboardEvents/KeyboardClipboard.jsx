9d5c445bebac55d99ba57b02597cfd6387f0a5ca
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-19 23:41:34 +0300

    Clipboard 2

--- src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 
 const KeyboardClipboard = () => {
+
+  const [inputValue, setInputValue] = useState("");
+
+console.log(inputValue)
   return <div className="container text-center">
     <h1>CLIPBOARD EVENTS</h1>
-    <input type="text" />
+    <input className="form-control" type="text" onChange={(e)=> setInputValue(e.target.value) 
+      }/>
+
+    <p>{inputValue}</p>
 
   </div>
   
