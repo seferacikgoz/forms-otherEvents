b0cb68de3bbb476bf072775dbed07ae6646efcdd
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-20 00:09:26 +0300

    clipboard 4

--- src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -3,14 +3,21 @@ import { useState } from "react";
 const KeyboardClipboard = () => {
 
   const [inputValue, setInputValue] = useState("");
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode );
+    if(e.keyCode >= 96 && e.keyCode <= 105){
+      alert("Please dont enter a number")
+    }
+  }
 
-console.log(inputValue)
   return <div className="container text-center">
     <h1>CLIPBOARD EVENTS</h1>
     <input className="form-control" type="text" onChange={(e)=> {
       e.target.value = e.target.value.toLocaleUpperCase()
       setInputValue(e.target.value)} 
-      }/>
+      }
+      onKeyDown={handleKeyDown}
+      />
 
     <p className="text-start mt-4">{inputValue.toLocaleUpperCase()}</p>
 
