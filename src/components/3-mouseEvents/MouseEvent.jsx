2833ba95f333deed169f2c407c3dc68250b4eb67
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-21 00:06:54 +0300

    Mouse Events

--- src/components/3-mouseEvents/MouseEvent.jsx
+++ src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
+
   const MouseEvent = () => {
+
+    const [showHint, setShowHint] = useState(false)
+
+    const handleDoubleClick = () => {};
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
 
       <div
         id="todo-1"
-        className="bg-success text-light w-50 p-4">
+        className="bg-success text-light w-50 p-4" 
+        onMouseEnter={() => setShowHint(true)}
+         onMouseOut={() => setShowHint(false)}>
         todo item 1
       </div>
 
+      {showHint && <span>Hello React</span>}
+      
+
       <div
         id="todo-2"
-        className="bg-success text-light w-50 p-4 mt-4">
+        className="bg-success text-light w-50 p-4 mt-4" onDoubleClick={handleDoubleClick}>
         todo item 2
       </div>
 
